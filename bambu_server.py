7460239cfe01d185a06c4ee8c634aa8b2c32b330
Robert Jones <rjones@fenixgate.com>
2026-03-19 10:42:27 -0400

    Fix Werkzeug allow_unsafe flag

--- bambu_server.py
+++ bambu_server.py
@@ -406,4 +406,4 @@ if __name__ == '__main__':
     threading.Thread(target=periodic_broadcast, daemon=True).start()
 
     log.info("Web server starting on port 5000")
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
